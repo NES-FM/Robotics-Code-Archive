4cd1af40efe08fbf93f6d1aa3ff4ae5eb6c9312a
xXFloCraftXx <flori.sonstwas@outlook.de>
2020-03-06 20:10:10 +0100

    Some Test-Files, most not important

---
+++ Sonstiges/drivers/firmwareVersion.h
@@ -0,0 +1,1 @@
+int kRobotCVersionNumeric = 360;
---
+++ VSCodeInclude.h
@@ -0,0 +1,11 @@
+int T1 = 0, T2 = 0, T3 = 0, T4 = 0, ll = 0, mm = 0, rr = 0;
+int time1[] = {0, 0, 0, 0};
+typedef void task;
+
+void clearDebugStream() {}
+void ClearTimer(int t) {}
+void StartTask(void (*t) ()) {}
+int SensorValue(int sensor) {return 0;}
+void nxtDisplayCenteredBigTextLine(int line, char* str) {};
+void nxtDisplayCenteredTextLine(int line, const char* format, int o = 0, int t = 0, int r = 0) {};
+void writeDebugStreamLine(const char* format, int o = 0, int t = 0, int r = 0) {};
